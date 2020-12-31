024b40c32e5da58a236c3e499deab0938226f80e
P_991173 <rafael.machado@camara.leg.br>
2020-12-31 16:17:59 -0300

    atualização

---
+++ guppe/dir_e_help.py
@@ -0,0 +1,13 @@
+"""
+Utilitários python para auxiliar na programação.
+
+Dir - Apresenta todos os atributos e funções/métodos disponíveis para
+determinado tipo de dado ou variável.
+
+dir(tipo de dado/variavel)
+
+Help - Apresenta a documentação/como utilizar os atributos/propriedaddes
+e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+help("selva".lower)
+"""
